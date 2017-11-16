3769157586ca0133f7ff76ce6e039e0bf90fab5c
Nathan JANCZEWSKI <nathan.janczewski@etudiant.univ-reims.fr>
2017-11-16 15:19:56 +0100

    Portage en PHP+twig pure de l'interface

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	require_once("vendor/autoload.php");
+
+	$loader = new \Twig_Loader_Filesystem(__DIR__ . '/twigs');
+	$twig   = new \Twig_Environment($loader);
+
+	echo $twig->render('index.html.twig');
