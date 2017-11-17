6707cdab5a664e1a66e2373025deccddb407bf28
Nathan JANCZEWSKI <nathan.janczewski@etudiant.univ-reims.fr>
2017-11-17 16:09:59 +0100

    TwigLoader fonctionnel, ajout automatique du formulaire de login

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-	require_once("autoload.inc.php");
+require_once("autoload.inc.php");
 
-	$loader = new \Twig_Loader_Filesystem(__DIR__ . '/twigs');
-	$twig   = new \Twig_Environment($loader);
+/**
+ * @TODO: Charger la liste des news et passer le tableau au twig
+ */
 
-
-	$loginForm = "";
-
-	if (Utilisateur::isConnected()){
-		$u = Utilisateur::createFromSession();
-		$loginForm = $u->createLogoutForm();
-	} else {
-		$loginForm = Utilisateur::createLoginForm();
-	}
-
-	echo $twig->render('index.html.twig', array('form' => $loginForm));
-
+echo TwigLoader::getInstance()->render('index.html.twig', array());
