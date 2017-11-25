9e0d7f2c1bb9a3c8e360b1fb34bcf520607d2854
Nathan JANCZEWSKI <nathan.janczewski@etudiant.univ-reims.fr>
2017-11-25 18:10:29 +0100

    Changement du nom des variables, plus lisible

--- index.php
+++ index.php
@@ -8,16 +8,16 @@ require_once("autoload.inc.php");
  * @TODO: Charger la liste des news et passer le tableau au twig
  */
 $MAX_PAGE_VIEW = 2;
-$listNews = Array();
-$numberOfPage = ceil(News::getCountNumbers() / $MAX_PAGE_VIEW);
+$listNews = array();
+$pageAmt = ceil(News::getCountNumbers() / $MAX_PAGE_VIEW);
 
-if (isset($_GET['pageNumber']) && !empty($_GET['pageNumber'])) {
-    $currentlyPage = $_GET['pageNumber'];
-    $star = (($_GET['pageNumber'] - 1) * $MAX_PAGE_VIEW);
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    $currentPage = $_GET['page'];
+    $star = (($_GET['page'] - 1) * $MAX_PAGE_VIEW);
     $listNews = News::createNewsNext($star, $MAX_PAGE_VIEW);
 
 } else {
-    $currentlyPage = 1;
+    $currentPage = 1;
     $listNews = News::createNewsNext(0, $MAX_PAGE_VIEW);
 
 }
@@ -25,4 +25,4 @@ if (isset($_GET['pageNumber']) && !empty($_GET['pageNumber'])) {
 /**
  * Rendu
  */
-echo TwigLoader::getInstance()->render('news', 'index', array('news' => $listNews, 'numberOfPage' => $numberOfPage, 'currentlyPage' => $currentlyPage));
+echo TwigLoader::getInstance()->render('news', 'index', array('news' => $listNews, 'pageAmt' => $pageAmt, 'currentPage' => $currentPage));
