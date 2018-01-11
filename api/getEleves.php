898ed588193f71fe497659ec4449d6e1efa0b2fd
LOUCIF Youness <youness.loucif@etudiant.univ-reims.fr>
2018-01-12 00:01:55 +0100

    Début de la gestion des absences (Professeur)
    Le front-end n'est pas resposive
    Le back-end ne fonctionne pas (CF : slack)

---
+++ api/getEleves.php
@@ -0,0 +1,51 @@
+<?php
+require_once "../autoload.inc.php";
+require_once 'verifAbsences.php';
+
+$id = $_GET['idUser'];
+$jour = $_GET['jour'];
+$mois = $_GET['mois'];
+$annee = $_GET['annee'];
+$heureD = $_GET['dheure'];
+$minuteD = $_GET['dminute'];
+$heureF = $_GET['fheure'];
+$minuteF = $_GET['fminute'];
+if(baseVerif($id) && baseVerif($jour) && baseVerif($mois) && baseVerif($annee) &&
+	baseVerif($heureD) && baseVerif($heureF) && baseVerif($minuteF) && baseVerif($minuteD) && isValidDay($jour,$mois,$annee) && coherence($heureD,$minuteD,$heureF,$minuteF)){
+	$stmt = myPDO::getInstance()->prepare(<<<SQL
+		SELECT *
+		FROM Utilisateur
+		WHERE idPersonne IN (SELECT idEtudiant
+							 FROM Etudiant
+							 WHERE idEtudiant IN ( SELECT idEtudiant
+							 						FROM Appartient
+							 						WHERE idGroupe
+		IN (SELECT idGroupe
+			FROM Seance
+			WHERE idProfesseur = :prof
+			AND dateDebut = :deb
+			AND dateFin  = :fin )))
+		ORDER BY nomPers, prenomPers
+SQL
+);
+	$stmt->bindParam(':prof',$id);
+	$begin = date('Y-m-d H:i:s',strtotime($annee.'-'.$mois.'-'.$jour.' '.$heureD.':'.$minuteD.':00'));
+	$end = date('Y-m-d H:i:s',strtotime($annee.'-'.$mois.'-'.$jour.' '.$heureF.':'.$minuteF.':00'));
+	$stmt->bindParam(':deb',$begin);
+	$stmt->bindParam(':fin',$end);
+	if(!$stmt->execute()){
+		throw new Exception();
+	}
+	$stmt->setFetchMode(PDO::FETCH_CLASS,"Utilisateur");
+	$temp = $stmt->fetchAll();
+	$eleves = array();
+	foreach ($temp as $key => $value) {
+		$eleves = array($eleves,$value);
+	}
+	var_dump($temp);
+	/*header("Content-type: application/json");
+	echo json_encode($eleves);*/
+} else {
+	http_response_code(400);
+	return;
+}
